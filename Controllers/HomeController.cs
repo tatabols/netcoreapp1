c62ce9058361ca3c20885d29bca9342120be454a
tatabols <tatabols@yahoo.com>
2021-12-10 17:53:40 +0800

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Azure.Core;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -20,6 +23,10 @@ namespace WebApplication1.Controllers
 
         public IActionResult Index()
         {
+            var s = new KeyvaultSecret();
+           var result =  s.GetSecret();
+            Console.WriteLine("result: "+ result);
+            ViewBag.secretvalue = result;
             return View();
         }
 
@@ -32,6 +39,44 @@ namespace WebApplication1.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+    }
+
+    public class KeyvaultSecret
+    {
+        public string GetSecret( )
+        {
+            try
+            {
+                Uri kvurl = new Uri("https://kvname100.vault.azure.net/secret1");
+                //scheme = protocol
+                string kvUri = kvurl.Scheme.ToString() + "://" + kvurl.Host.ToString();
+                //second section of url which is after the domain name
+                string secretName = kvurl.Segments[1];
+                SecretClientOptions options = new SecretClientOptions()
+                {
+                    Retry =
+                        {
+                            Delay= TimeSpan.FromSeconds(1),
+                            MaxDelay = TimeSpan.FromSeconds(16),
+                            MaxRetries = 2,
+                            Mode = RetryMode.Exponential
+                        }
+                };
+
+                var client = new SecretClient(new Uri(kvUri), new DefaultAzureCredential(), options);
+                KeyVaultSecret secretBundle = client.GetSecret(secretName);
+                string secret = secretBundle.Value;
+                Console.WriteLine(secret);
+
+                return "secret from vaault: "+secret;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            return "no secret due to error";
         }
     }
 }
